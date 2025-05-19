a47f373065b5298c280a4d83dee4f79bfff1e72f
shahid <shahid.ali5@wipro.com>
2025-05-19 09:30:20 +0530

    Unit test

---
+++ tests/test_operation.py
@@ -0,0 +1,14 @@
+from src.math.operations import add,sub 
+
+def test_add():
+      assert add(2,3)==5
+      assert add(4,4)==8
+      
+def test_sub():
+      assert(5,3)==2
+      assert(5,2)==3
+      assert(4,2)==2
+      assert(1,-1)==0
+      
+      
+      
